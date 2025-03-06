cc4d0c914fc2fbac73196176f01d67356f2269d8
Boyd Querubin <boydisaacq@gmail.com>
2025-03-06 08:38:48 -0700

    ceil

---
+++ week1/boxes.py
@@ -0,0 +1,7 @@
+manufactured_items = int("Enter the number of items: ")
+
+items_pbox = int("Enter the number of items per box: ")
+
+
+
+print(f"For {manufactured_items} items, packing {items_pbox} items in each box, you will need ")
