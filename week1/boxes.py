202873393cc2bc9ef9aa9cc0d63074e8c322bb76
Boyd Querubin <boydisaacq@gmail.com>
2025-03-06 12:46:52 -0700

    prep 2

--- week1/boxes.py
+++ week1/boxes.py
@@ -1,7 +1,9 @@
-manufactured_items = int("Enter the number of items: ")
+import math
 
-items_pbox = int("Enter the number of items per box: ")
+manufactured_items = int(input("Enter the number of items: "))
 
+items_pbox = int(input("Enter the number of items per box: "))
 
+n_box = math.ceil(manufactured_items / items_pbox)
 
-print(f"For {manufactured_items} items, packing {items_pbox} items in each box, you will need ")
+print(f"For {manufactured_items} items, packing {items_pbox} items in each box, you will need {n_box} boxes")
