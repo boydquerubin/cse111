60f930b2a91035ac2ee674205bdcb87d19b3cda7
Boyd Querubin <boydisaacq@gmail.com>
2025-03-12 11:51:26 -0600

    parameters

--- week2/learn1_2.py
+++ week2/learn1_2.py
@@ -5,8 +5,11 @@
 
 # print("Your initials are: " + first_name_initial.upper() + last_name_initial.upper())
 
-def get_initial(name):
-  initial = name[0:1].upper()
+def get_initial(name, force_uppercase=True):
+  if force_uppercase:    
+    initial = name[0:1].upper()
+  else:
+    initial = name[0:1]
   return initial
 
 first_name = input("Enter your first name: ")
@@ -15,3 +18,4 @@ last_name = input("Enter your first name: ")
 last_name_initial = get_initial(last_name)
 
 print("Your initials are: " + first_name_initial + last_name_initial)
+
