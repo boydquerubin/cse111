412a6428a8b3261191174ef209637e57dc9e3c3b
Boyd Querubin <boydisaacq@gmail.com>
2025-03-11 18:47:03 -0600

    learning

---
+++ week2/learn1_1.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+def print_time():
+  print('task completed')
+  print(datetime.now())
+
+first_name = 'Boyd'
+print_time()
+
+for x in range(0, 10):
+  print(x)
+print_time()
---
+++ week2/learn1_2.py
@@ -0,0 +1,17 @@
+# first_name = input("Enter your first name: ")
+# first_name_initial = first_name[0:1]
+# last_name = input("Enter your first name: ")
+# last_name_initial = last_name[0:1]
+
+# print("Your initials are: " + first_name_initial.upper() + last_name_initial.upper())
+
+def get_initial(name):
+  initial = name[0:1].upper()
+  return initial
+
+first_name = input("Enter your first name: ")
+first_name_initial = get_initial(first_name)
+last_name = input("Enter your first name: ")
+last_name_initial = get_initial(last_name)
+
+print("Your initials are: " + first_name_initial + last_name_initial)
