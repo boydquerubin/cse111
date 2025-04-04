12590406735691799db2bc24f8b7253f58868a4c
Boyd Querubin <boydisaacq@gmail.com>
2025-04-04 09:47:21 -0600

    test

--- week5/receipt.py
+++ week5/receipt.py
@@ -1,3 +1,13 @@
+# Import the datetime class from the datetime
+# module so that it can be used in this program.
+from datetime import datetime
+# Call the now() method to get the current
+# date and time as a datetime object from
+# the computer's operating system.
+current_date_and_time = datetime.now()
+# Use an f-string to print the current
+# day of the week and the current time.
+
 import csv
 
 PRODUCT_ID = 0
@@ -8,30 +18,60 @@ PRODUCT_PRICE_INDEX = 2
 
 
 def main():
-    KEY_COLUMN_INDEX = 0
-    products_dict = read_dictionary("products.csv", KEY_COLUMN_INDEX)
-    print("All Products")
-    print(products_dict)
-    print("Requested Items:")
+    try:
+        KEY_COLUMN_INDEX = 0
+        products_dict = read_dictionary("products.csv", KEY_COLUMN_INDEX)
+        print("Albertsons")
+        # print("All Products")
+        # print(products_dict)
+        # print("Requested Items:")    
 
-    with open("request.csv", "rt") as request_file:
-        reader = csv.reader(request_file)
-        next(reader)
+        with open("request.csv", "rt") as request_file:
+            reader = csv.reader(request_file)
+            next(reader)
 
-        for row_list in reader:
-            product_number = row_list[PRODUCT_ID]
-            quantity = int(row_list[PRODUCT_QUANTITY])
+            items = 0
+            subtotal = 0             
 
-            if product_number in products_dict:
-                product_info = products_dict[product_number]
-                product_name = product_info[PRODUCT_NAME_INDEX]
-                product_price = float(product_info[PRODUCT_PRICE_INDEX])
+            for row_list in reader:
+                product_number = row_list[PRODUCT_ID]
+                quantity = int(row_list[PRODUCT_QUANTITY])
 
-                print(f"{product_name}: {quantity} @ ${product_price:.2f}")
-            else:
-                print(f"Warning: Product ID '{product_number}' not found in product list.")
-                
-    print(f"{request_file}")
+                if product_number in products_dict:
+                    product_info = products_dict[product_number]
+                    product_name = product_info[PRODUCT_NAME_INDEX]
+                    product_price = float(product_info[PRODUCT_PRICE_INDEX])
+
+                    total_item_price = product_price * quantity
+
+                    print(f"{product_name}: {quantity} @ ${product_price:.2f}")
+                else:
+                    print(f"Warning: Product ID '{product_number}' not found in product list.")
+
+                if quantity > 0:
+                    items += quantity
+
+                if product_price > 0:
+                    subtotal += total_item_price
+                    tax = subtotal * .06
+                    grand_total = subtotal + tax    
+        
+    except FileNotFoundError as not_found_err:
+        print(not_found_err)
+
+    except PermissionError as perm_err:
+        print(perm_err)
+
+    except KeyError as key_err:
+        print(type(key_err).__name__, key_err)
+            
+    # print(f"{request_file}")
+    print(f"Number of items: {items}")
+    print(f"Subtotal: ${subtotal:.2f}")
+    print(f"Tax: ${tax:.2f}")
+    print(f"Total: ${grand_total:.2f}")
+    print("Thank you for shopping at Albertsons.")
+    print(f"{current_date_and_time:%c}")
 
 def read_dictionary(filename, key_column_index):
     """
