2fde681a695d131aa50f6c5f04543517e33e2d26
Boyd Querubin <boydisaacq@gmail.com>
2025-04-05 08:20:27 -0600

    start

--- project/workouts.py
+++ project/workouts.py
@@ -2,11 +2,22 @@ import csv
 import random
 from datetime import datetime, timedelta
 
-current_date_and_time = datetime.now()
+current_date_and_time = datetime.today()
+
+WORKOUT_DAY_INDEX = 0
+WORKOUT_MUSCLE_GROUP_INDEX = 1
+WORKOUT_INDEX = 2
 
 def main():
     print("Workout Generator")
-    print(f"{current_date_and_time:%c}")
+    # print(f"{current_date_and_time:%c}")
+    with open("workouts.csv", "rt") as dentists_file:
+        reader = csv.reader(dentists_file)
+        next(reader)
+        for row_list in reader:
+            print(row_list)
+
+# def get_day():
 
 if __name__ == "__main__":
     main()
