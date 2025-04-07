beed15e27f020821027ffacc442b07e3de3ea67a
Boyd Querubin <boydisaacq@gmail.com>
2025-04-07 15:01:02 -0600

    do tests

--- project/workouts.py
+++ project/workouts.py
@@ -8,31 +8,47 @@ WORKOUT_INDEX = 2
 
 def main():
     print("Workout Generator")
-    print("Today is:", get_day())
-    # print(get_workouts_for_day(day))    
+    day = get_day()
+    today_workouts = get_workouts_for_day(day)
+    randomized = generate_workout(today_workouts, 6)
+    display_workout(day, randomized)
 
 def get_day():
     today = datetime.today()
     return today.strftime("%A")
 
 def get_workouts_for_day(day):
-    with open("workouts.csv", "rt") as dentists_file:
-        reader = csv.reader(dentists_file)
+    with open("workouts.csv", "rt") as workouts_file:
+        reader = csv.reader(workouts_file)
         next(reader)
+        workouts_for_today = []
+
         for row_list in reader:
-            day = row_list[WORKOUT_DAY_INDEX]
+            workout_day = row_list[WORKOUT_DAY_INDEX]
             muscle_group = row_list[WORKOUT_MUSCLE_GROUP_INDEX]
             workout = row_list[WORKOUT_INDEX]
-            print(f"Day: {day}, Muscle group: {muscle_group}, Workout: {workout}")
 
-# def generate_workout(workout_list, count):
+            if workout_day == day:
+                workouts_for_today.append((muscle_group, workout))
 
-# def display_workout(day, selected_workouts):
+        return workouts_for_today
 
-# get_workouts_for_day(day) – returns list of workouts based on the day
-# generate_workout(workout_list, count) – randomly selects a given number of workouts
-# display_workout(day, selected_workouts) – prints the final workout in a clean format
-# main() – the main driver of the program
+def generate_workout(workout_list, count):
+    if count > len(workout_list):
+        count = len(workout_list)
+
+    workouts_randomized = random.sample(workout_list, count)
+    return workouts_randomized
+
+def display_workout(day, selected_workouts):
+    print("")
+    print(f"Today is {day}.")
+    print(f"Here is a list of {day}'s workouts:")
+    print("")
+    counter = 1
+    for muscle_group, workout in selected_workouts:
+        print(f"{counter}: {muscle_group}: {workout}")
+        counter += 1
 
 if __name__ == "__main__":
     main()
