cd5a86104c39cbf076cf7e4118a74b2700ca405e
Boyd Querubin <boydisaacq@gmail.com>
2025-04-07 08:33:36 -0600

    d1

--- project/workouts.py
+++ project/workouts.py
@@ -1,8 +1,6 @@
 import csv
 import random
-from datetime import datetime, timedelta
-
-current_date_and_time = datetime.today()
+from datetime import datetime
 
 WORKOUT_DAY_INDEX = 0
 WORKOUT_MUSCLE_GROUP_INDEX = 1
@@ -10,14 +8,31 @@ WORKOUT_INDEX = 2
 
 def main():
     print("Workout Generator")
-    # print(f"{current_date_and_time:%c}")
+    print("Today is:", get_day())
+    # print(get_workouts_for_day(day))    
+
+def get_day():
+    today = datetime.today()
+    return today.strftime("%A")
+
+def get_workouts_for_day(day):
     with open("workouts.csv", "rt") as dentists_file:
         reader = csv.reader(dentists_file)
         next(reader)
         for row_list in reader:
-            print(row_list)
+            day = row_list[WORKOUT_DAY_INDEX]
+            muscle_group = row_list[WORKOUT_MUSCLE_GROUP_INDEX]
+            workout = row_list[WORKOUT_INDEX]
+            print(f"Day: {day}, Muscle group: {muscle_group}, Workout: {workout}")
 
-# def get_day():
+# def generate_workout(workout_list, count):
+
+# def display_workout(day, selected_workouts):
+
+# get_workouts_for_day(day) – returns list of workouts based on the day
+# generate_workout(workout_list, count) – randomly selects a given number of workouts
+# display_workout(day, selected_workouts) – prints the final workout in a clean format
+# main() – the main driver of the program
 
 if __name__ == "__main__":
     main()
